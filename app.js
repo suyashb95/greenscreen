392a21344ccf8e250b518d9ee089f71054015a68
Sam Radhakrishnan <sk09idm@gmail.com>
2020-09-12 14:38:45 +0530

    Fix bug with segmentor.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const WebCam = require('./lib/webcam.js');
 const Segmentor = require('./lib/segmentor.js');
 const logger = require('./utils/logger.js');
 
-const outputLocation = 'data/webcam/shot.jpg';
+const location = 'data/webcam/shot.jpg';
 const webcam = new WebCam(1280, 780, 30, logger);
 const segmentor = new Segmentor(logger);
 
-webcam.capture(outputLocation);
-const segmentation = segmentor.segmentImage(outputLocation);
-// eslint-disable-next-line no-console
-console.log(segmentation);
+async function main(outputLocation) {
+    webcam.capture(outputLocation);
+    const segmentation = await segmentor.segmentImage(outputLocation);
+    // eslint-disable-next-line no-console
+    console.log(segmentation);
+}
+main(location);
