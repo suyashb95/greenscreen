ea9793014082d3c4dd681e3e794eb8878c8ca4a6
Sam Radhakrishnan <sk09idm@gmail.com>
2020-09-12 12:37:12 +0530

    Add webcam shots directory to .gitignore

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const WebCam = require('./lib/webcam.js');
 
-const outputLocation = 'data/webcamshot.jpg';
+const outputLocation = 'data/webcam/shot.jpg';
 const webcam = new WebCam(1280, 780, 30);
 webcam.capture(outputLocation);
