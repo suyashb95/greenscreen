d1d590b07c83d97308bceaca0d35abc139d40858
Suyash458 <suyash.behera458@gmail.com>
2020-09-12 16:59:30 +0530

    add canvas. convert image buffer for segmentation

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const WebCam = require('./lib/webcam.js');
 const Segmentor = require('./lib/segmentor.js');
 const logger = require('./utils/logger.js');
 
-const location = 'data/webcam/shot.jpg';
+const location = './data/webcam/shot.jpg';
 const webcam = new WebCam(1280, 780, 30, logger);
 const segmentor = new Segmentor(logger);
+segmentor.initializeNet();
 
 async function main(outputLocation) {
-    webcam.capture(outputLocation);
-    const segmentation = await segmentor.segmentImage(outputLocation);
+    webcam.capture(outputLocation)
+    .then(image => {
+        segmentor.segmentImage(image);
+    })
     // eslint-disable-next-line no-console
-    console.log(segmentation);
 }
+
 main(location);
