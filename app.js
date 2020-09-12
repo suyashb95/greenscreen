2d53605b48cb991e62d45aa29217028a13e98977
Sam Radhakrishnan <sk09idm@gmail.com>
2020-09-12 14:25:07 +0530

    Add a segmentor class to segment video

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const WebCam = require('./lib/webcam.js');
+const Segmentor = require('./lib/segmentor.js');
+const logger = require('./utils/logger.js');
 
 const outputLocation = 'data/webcam/shot.jpg';
-const webcam = new WebCam(1280, 780, 30);
+const webcam = new WebCam(1280, 780, 30, logger);
+const segmentor = new Segmentor(logger);
+
 webcam.capture(outputLocation);
+const segmentation = segmentor.segmentImage(outputLocation);
+// eslint-disable-next-line no-console
+console.log(segmentation);
